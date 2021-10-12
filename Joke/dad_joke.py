a448b47b35caaf327faac27d1650858b69555501
lem0n4id <61219881+lem0n4id@users.noreply.github.com>
2021-10-12 16:54:51 +0530

    Update dad_joke.py
    removed colored function from termcolor library since it doesnt work on windows

--- Joke/dad_joke.py
+++ Joke/dad_joke.py
@@ -1,12 +1,10 @@
 from pyfiglet import figlet_format
-from termcolor import colored
 from requests import get
 from random import choice
 
 url = "https://icanhazdadjoke.com/search"
 defualt_text = figlet_format("Dad Joke Project", font="smslant")
-colored_text = colored(defualt_text, color="green")
-print(colored_text)
+print(defualt_text)
 
 
 def print_joke():
@@ -21,14 +19,14 @@ def print_joke():
 
     data = response.json()
     if data['total_jokes'] == 0:
-        print(colored("Please enter valid search term",
-              'red', 'on_cyan', attrs=['concealed']))
+        print("Please enter valid search term",
+              'red', 'on_cyan', attrs=['concealed'])
         print_joke()
     elif data['total_jokes'] > 0:
         print(
             f"You have fetched {data['total_jokes']} jokes and among those below joke is random one\n")
-        print(colored(choice(data['results'])['joke'],
-              'cyan', 'on_grey', attrs=['bold']))
+        print(choice(data['results'])['joke'])
+        print()
 
 
 print_joke()
